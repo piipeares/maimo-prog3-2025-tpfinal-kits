5347bceed935b0b28b2ab481d3607313c2092b0a
piipeares <piipeares@gmail.com>
2025-11-20 19:55:07 -0300

    ultimos cambios home

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import { KitProvider } from "./context/KitContext";
 import { FavoritesProvider } from "./context/FavoritesContext";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer"; 
 
 export const metadata = {
   title: "KitRadar",
@@ -12,7 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-neutral-950 text-neutral-100 flex flex-col min-h-screen"> {/* Flex col para empujar el footer */}
+      {/* Agregada la clase 'overflow-x-hidden' para eliminar el scroll horizontal */}
+      <body className="bg-neutral-950 text-neutral-100 flex flex-col min-h-screen overflow-x-hidden">
         <KitProvider>
           <FavoritesProvider>
             <Navbar />
@@ -20,7 +21,9 @@ export default function RootLayout({ children }) {
             <div className="mx-auto max-w-7xl px-4 py-8 w-full flex-1">
               {children}
             </div>
+
             <Footer /> 
+            
           </FavoritesProvider>
         </KitProvider>
       </body>
